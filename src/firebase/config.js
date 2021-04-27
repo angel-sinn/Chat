75086e42ad617437696bf57072972e10a4308096
Angel <angelkysinn@gmail.com>
2021-04-27 14:35:36 -0700

    Initial setup

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChTJrSaORCI6RxmaOwF-SpdDuOaP0KsLg",
+  authDomain: "chat-app-855de.firebaseapp.com",
+  projectId: "chat-app-855de",
+  storageBucket: "chat-app-855de.appspot.com",
+  messagingSenderId: "469447475329",
+  appId: "1:469447475329:web:a65498e49016e1ee138d80"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
