76faf09271a69eea1b4042363bade1f6193895b9
Angel <angelkysinn@gmail.com>
2021-04-27 14:46:48 -0700

    Set up login and signup form

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="Display Name" v-model="displayName">
+    <input type="email" required placeholder="Email" v-model="email">
+    <input type="password" required placeholder="Password" v-model="password">
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+
+export default {
+  setup() {
+    const displayName = ref("")
+    const email = ref("")
+    const password = ref("")
+
+    const handleSubmit = () => {
+      console.log(displayName.value, email.value, password.value);
+    }
+
+    return { displayName, email, password, handleSubmit }
+  }
+}
+</script>
