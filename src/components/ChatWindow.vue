5d2b36ca5c077c549e1e51d8512a47bf6b2365df
Angel <angelkysinn@gmail.com>
2021-04-27 21:48:35 -0700

    Add function to unsubscribe from real-time listener for performance

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="chat-window">
     <div v-if="error">{{ error }}</div>
-    <div v-if="documents" class="messages">
+    <div v-if="documents" class="messages" ref="messages">
       <div v-for="document in formattedDocuments" :key="document.id" class="chat">
         <span class="name">{{ document.name }}:</span>
         <span class="message">{{ document.message }}</span>
@@ -12,7 +12,7 @@
 </template>
 
 <script>
-import { computed } from '@vue/runtime-core';
+import { computed, onUpdated, ref } from '@vue/runtime-core';
 
 import getCollection from '../composables/getCollection';
 import { formatDistanceToNow } from 'date-fns';
@@ -31,7 +31,14 @@ export default {
       }
     })
 
-    return { error, documents, formattedDocuments }
+    // auto-scroll to bottom of messages
+    const messages = ref(null);
+
+    onUpdated(() => {
+      messages.value.scrollTop = messages.value.scrollHeight
+    });
+
+    return { error, documents, formattedDocuments, messages };
   }
 }
 </script>
--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,3 +1,4 @@
+import { watchEffect } from "vue";
 import { ref } from "@vue/reactivity";
 import { projectFirestore } from "../firebase/config";
 
@@ -9,19 +10,26 @@ const getCollection = (collection) => {
     .orderBy('createdAt');
   
   // real-time listener
-  collectionRef.onSnapshot((snap) => {
+  const unsub = collectionRef.onSnapshot((snap) => {
     let results = [];
     snap.docs.forEach((doc) => {
       // timestamp not created until it reaches server, that's why need the doc.data().createdAt check
       doc.data().createdAt && results.push({ ...doc.data(), id: doc.id });
     })
+
+    // update values
     documents.value = results;
     error.value = null;
   }, (err) => {
     console.log(err.message);
-
-    documents.value = null;
+    documents.value - null;
     error.value = 'Could not fetch data';
+  });
+
+  // onInvalidate will run when component unmounts. Can unsubscribe from real-time listeners
+  watchEffect((onInvalidate) => {
+    // unsub from prev collection when watcher is stopped (component unmounted)
+    onInvalidate(() => unsub())
   });
 
   return { documents, error };
