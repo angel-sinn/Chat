e8669abe93bd9f3c7746ba124a2920bd5b2c8ede
Angel <angelkysinn@gmail.com>
2021-04-27 17:39:48 -0700

    Create chat form component

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="container">
     <Navbar/>
+    <NewChatForm/>
   </div>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue'
+import { watch } from '@vue/runtime-core';
+import { useRouter } from 'vue-router';
+
+import Navbar from '../components/Navbar.vue';
+import NewChatForm from '../components/NewChatForm.vue';
+import getUser from '../composables/getUser';
 
 export default {
-  components: { Navbar }
-}
+  components: { Navbar, NewChatForm },
+  setup() {
+    const { user } = getUser();
+    const router = useRouter();
+
+    // watch user obj
+    watch(user, () => {
+      // user is a ref, need to use user.value
+      if (!user.value) {
+        router.push({ name: 'Welcome' });
+      }
+    });
+
+    return { user };
+  }
+};
 </script>
-
-<style>
-
-</style>
